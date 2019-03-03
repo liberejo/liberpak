c8822e349f7e4c96e2036cd8d6f75afe84c7bf9e
Cassaundra <cass@cassaundra.io>
2019-03-02 19:33:26 -0800

    Setup Gradle dependencies and application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,26 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
+    application
     kotlin("jvm") version "1.3.21"
 }
 
 group = "org.liberejo"
 version = "0.1.0-SNAPSHOT"
 
+application {
+    mainClassName = "org.liberejo.launcher.DesktopLauncher"
+}
+
 repositories {
     mavenLocal()
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    compile("org.liberejo:api:0.1.0-SNAPSHOT")
+    implementation("org.liberejo:launcher:0.1.0-SNAPSHOT")
+    implementation("org.liberejo:api:0.1.0-SNAPSHOT")
     testCompile("junit", "junit", "4.12")
 }
 
---
+++ src/main/kotlin/org/liberejo/liberpak/Liberpak.kt
@@ -0,0 +1,19 @@
+package org.liberejo.liberpak
+//
+//import org.kodein.di.Kodein
+//import org.liberejo.api.mod.plugin.LiberejoPlugin
+//
+//class Liberpak : LiberejoPlugin() {
+//    override val kodein: Kodein
+//        get() = TODO("not implemented")
+//
+//    override fun onLoad() {
+//        TODO("not implemented")
+//
+//    }
+//
+//    override fun onUnload() {
+//        TODO("not implemented")
+//    }
+//
+//}
