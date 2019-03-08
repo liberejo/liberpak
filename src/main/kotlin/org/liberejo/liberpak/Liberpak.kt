f286100aa235afb4a4b592fa59b1a16f1587529e
Cassaundra <cass@cassaundra.io>
2019-03-07 16:30:52 -0800

    Add liberejo-package.json and minimal plugin

--- src/main/kotlin/org/liberejo/liberpak/Liberpak.kt
+++ src/main/kotlin/org/liberejo/liberpak/Liberpak.kt
@@ -1,19 +1,15 @@
 package org.liberejo.liberpak
-//
-//import org.kodein.di.Kodein
-//import org.liberejo.api.mod.plugin.LiberejoPlugin
-//
-//class Liberpak : LiberejoPlugin() {
-//    override val kodein: Kodein
-//        get() = TODO("not implemented")
-//
-//    override fun onLoad() {
-//        TODO("not implemented")
-//
-//    }
-//
-//    override fun onUnload() {
-//        TODO("not implemented")
-//    }
-//
-//}
+
+import com.badlogic.gdx.Gdx
+import org.kodein.di.Kodein
+import org.liberejo.api.mod.plugin.LiberejoPlugin
+
+class Liberpak(override val kodein: Kodein) : LiberejoPlugin() {
+    override fun onLoad() {
+        Gdx.app.log("Liberepak", "Loaded")
+    }
+
+    override fun onUnload() {
+        Gdx.app.log("Liberpak", "Unloaded")
+    }
+}
